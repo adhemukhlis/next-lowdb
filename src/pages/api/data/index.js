6475897f173bce79b4275e55779a66c77fb03665
adhemukhlis <adhemukhlis@gmail.com>
2024-02-22 23:13:06 +0700

    update

--- src/pages/api/data/index.js
+++ src/pages/api/data/index.js
@@ -1,11 +1,13 @@
 // import globalStore from "@/db/globalStore"
-import { readFile } from '@/db/jsondb'
+// import { readFile } from '@/db/jsondb'
+const fs = require("fs");
 
 const mock = async (req, res) => {
 	switch (req.method) {
 		case 'GET': {
 			// const data = globalStore.get('data')
-			const data = await readFile()
+			// const data = await readFile()
+			const data = fs.readdirSync('/vercel/path0/src/db');
 			return res.status(200).json({
 				message: `get data success!`,
 				data
